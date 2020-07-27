15ae6d59e8f7fec90e7f1257fb1014aba5668106
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-07-26 22:27:48 -0300

    feat(image-header): Finished component.

--- InstaClone/src/Components/ImageHeader/index.js
+++ InstaClone/src/Components/ImageHeader/index.js
@@ -2,19 +2,20 @@ import React, { Fragment } from 'react';
 import {
   Text,
   Image,
+  View,
 } from 'react-native';
 
 import style from './style';
 
 const ImageHeader = ({ username }) => {
   return (
-    <Fragment>
+    <View style={style.header}>
       <Image
         source={require('../../../res/img/alura.jpg')}
         style={style.userPhoto}
       />
       <Text>{username}</Text>
-    </Fragment>
+    </View>
   );
 }
 
--- InstaClone/src/Components/ImageHeader/style.js
+++ InstaClone/src/Components/ImageHeader/style.js
@@ -4,7 +4,13 @@ const style = StyleSheet.create({
   userPhoto: {
     width: 40,
     height: 40,
-  }
+    margin: 10,
+    borderRadius: 30,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
 });
 
 export default style;
