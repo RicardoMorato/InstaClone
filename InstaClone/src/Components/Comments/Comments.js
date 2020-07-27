3b1cd69523a09ee62539e99c8528e1e0a30e6245
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-07-27 00:30:55 -0300

    feat(comments): Fixed textInputField.clear().

--- InstaClone/src/Components/Comments/Comments.js
+++ InstaClone/src/Components/Comments/Comments.js
@@ -1,22 +1,59 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import {
   Text,
   FlatList,
+  TextInput,
+  Image,
+  View,
+  TouchableOpacity,
 } from 'react-native';
 
 import style from './style';
 
 const Comments = ({ comments }) => {
-  return (
-    <FlatList
-      data={comments}
-      keyExtractor={(item, index) => index.toString()}
-      renderItem={({ item }) =>
-        <Text>{item.text}</Text>
-      }
-    >
+  const [commentsState, setCommentsState] = useState(comments);
+  let textInputContent = '';
+  let textInputField;
 
-    </FlatList>
+  const addComment = () => {
+    textInputField.clear();
+    const newComment = {
+      date: Date.now(),
+      text: textInputContent,
+      userName: "RicardoMorato",
+    };
+
+    setCommentsState([...commentsState, newComment]);
+  };
+
+  return (
+    <Fragment>
+      <FlatList
+        data={commentsState}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) =>
+          <View style={style.addComments}>
+            <Text style={style.userName} >{item.userName}</Text>
+            <Text style={style.comments} >{item.text}</Text>
+          </View>
+        }
+      />
+
+      <View style={style.addComments}>
+        <TextInput
+          ref={textInput => textInputField = textInput}
+          onChangeText={text => textInputContent = text}
+          placeholder={'Leave your comment here'}
+          style={{ flex: 1, }}
+        />
+        <TouchableOpacity onPress={addComment} >
+          <Image
+            style={style.imgSend}
+            source={require('../../../res/img/send.png')}
+          />
+        </TouchableOpacity>
+      </View>
+    </Fragment>
   );
 }
 
---
+++ InstaClone/src/Components/Comments/style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const style = StyleSheet.create({
+  userName: {
+    fontWeight: "bold",
+  },
+  comments: {
+    marginLeft: 5,
+  },
+  addComments: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+  imgSend: {
+    width: 25,
+    height: 25,
+    marginRight: 15,
+  },
+});
+
+export default style;
