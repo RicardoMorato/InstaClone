2a0f60a6431513390e07dbdf653a6e0e51132e0e
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-07-30 22:49:25 -0300

    feat(platform): Change http request to know platform.

--- InstaClone/src/apiCommunication/feed.js
+++ InstaClone/src/apiCommunication/feed.js
@@ -1,5 +1,11 @@
+import { Platform } from 'react-native';
+
 const getInfo = async (callback) => {
-  const infoHTTP = await fetch('http://10.0.2.2:3030/feed');
+  let url  = '10.0.2.2';
+  if (Platform.OS === 'ios') {
+    url = 'localhost';
+  }
+  const infoHTTP = await fetch(`http://${url}:3030/feed`);
   const infoJson = await infoHTTP.json();
   callback(infoJson);
 };
